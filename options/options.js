ac20d75ed899d532fed4ea2c3e4bda55b9f5cb9d
Crowdbotics <engineering@crowdbotics.com>
2023-05-16 23:10:34 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "twilight-firefly-de-83884",
+  url: "https://twilight-firefly-de-83884.botics.co",
+  api: "https://twilight-firefly-de-83884.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
